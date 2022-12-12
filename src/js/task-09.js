ce348dad9bd4b9bfc662cf34402240198cdb9b19
Kornilov <110562214+Dimar95@users.noreply.github.com>
2022-12-12 18:04:45 +0200

    tesk - 09

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,16 @@
+const btnRef = document.querySelector('.change-color');
+const spanRef = document.querySelector('.color');
+const bodyRef = document.querySelector('body');
+
+console.log("🎅 ~ btnRef", btnRef, spanRef)
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+btnRef.addEventListener('click', () => {
+  const color = getRandomHexColor();
+  spanRef.textContent = `${color}`;
+bodyRef.style.backgroundColor = `${color}`;
+})
