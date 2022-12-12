b059c4e3c9fb7ec4dcf511fa2029bde6dae7992a
Kornilov <110562214+Dimar95@users.noreply.github.com>
2022-12-12 23:05:56 +0200

    task - 10

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,7 +1,6 @@
 const inputRef = document.querySelector('#font-size-control');
 const spanRef = document.querySelector('#text');
-console.log("🎅 ~ spanRef", spanRef)
-console.dir(inputRef)
+
 inputRef.addEventListener('input', () =>  spanRef.style.fontSize = `${inputRef.valueAsNumber}px`)
 
 
--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,60 @@
+const btnCreateRef = document.querySelector('button[data-create]');
+// console.log("🎅 ~ btnCreateRef", btnCreateRef)
+const btnDestroyRef = document.querySelector('button[data-destroy]');
+// console.log("🎅 ~ btnDestroyRef", btnDestroyRef)
+const divBoxRef = document.querySelector('#boxes');
+// console.log("🎅 ~ divBoxRef", divBoxRef)
+const divRef = document.querySelector('#controls');
+const inputRef = divRef.firstElementChild;
+const divElArray = [];
+let colectionBox = document.querySelector('.item');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function createBoxes(amount) {
+  console.log('amount', amount)
+  for (let i = 0; i < amount; i++) {
+    let withBox = 0;
+    withBox += 30 + ([i] * 10)
+    let color = getRandomHexColor()
+
+    const divEl = document.createElement('div');
+    divEl.classList.add('item');
+
+    divEl.style.width = `${withBox}px`;
+    divEl.style.height = `${withBox}px`;
+    divEl.style.backgroundColor = `${color}`;
+    divEl.style.margin = '10px';
+    divEl.style.display = 'inline-flex';
+
+    divElArray.push(divEl);
+    
+  }
+  return divBoxRef.append(...divElArray);
+}
+
+
+let totalBox;
+inputRef.addEventListener('input', () => {
+  console.log("🎅 ~ inputRef.value", inputRef.value)
+  console.log("🎅 ~ totalBox", totalBox)
+  return totalBox = inputRef.valueAsNumber;
+  
+})
+  
+  
+
+btnCreateRef.addEventListener('click', () => createBoxes(totalBox));
+
+btnDestroyRef.addEventListener('click', () => destroyBoxes());
+
+
+function destroyBoxes() {
+  divBoxRef.innerHTML = '';
+  divElArray.splice(0, divElArray.length)
+}
+
+
+// valueAsNumber
